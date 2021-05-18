fac84ec43ab61dbbd4f6c1bd2f851ab7b67f05a2
dindin-glitch <66629792+dindin-glitch@users.noreply.github.com>
2021-05-18 12:03:54 +0200

    Update macos-updater.rb

--- Formula/macos-updater.rb
+++ Formula/macos-updater.rb
@@ -2,7 +2,7 @@ class MacosUpdater < Formula
   desc "Checks homebrew, macports, pkgsrc or nix, and updates outdated installations"
   homepage "https://github.com/dindin-glitch/macos-updater"
   url "https://github.com/dindin-glitch/macos-updater/archive/1.0.5.tar.gz"
-  sha256 "78c072e050c124e23317d8d1f8c7d7ce7e407d1a154f1465bb26c3f073db18e5"
+  sha256 "a1e08ebcb7eb8262dfa3196139dbdff3237cc02a538447a2a34d444ef38936fd"
   license "BSD-2-Clause"
 
   bottle :unneeded  
