d6daec47801a7df3163e0d66231fd48534162a54
dindin-glitch <66629792+dindin-glitch@users.noreply.github.com>
2021-05-23 20:54:38 +0200

    Update pkgmgr-updater.rb

--- Formula/pkgmgr-updater.rb
+++ Formula/pkgmgr-updater.rb
@@ -1,7 +1,7 @@
 class PkgmgrUpdater < Formula
   desc "Checks homebrew, macports, pkgsrc or nix, and updates outdated installations"
   homepage "https://github.com/dindin-glitch/pkgmgr-updater"
-  url "https://github.com/dindin-glitch/pkgmgr-updater/archive/1.0.6.tar.gz"
+  url "https://github.com/dindin-glitch/pkgmgr-updater/archive/1.1.0.tar.gz"
   sha256 "fc7fd32f9c610ff85629c847a6ce921626a97a1afdfdf1bddfd8bb8cf33f405b"
   license "BSD-2-Clause"
 
